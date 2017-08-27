e34445a83c063f9166df10f0779c639aca223bf2
nelson.davenapalli <nelson.csits@gmail.com>
2017-08-27 18:44:17 +0200

    Modify the webpage title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My first page using angular';
+  title = 'My first page using angular. Amazing indeed.';
 }
